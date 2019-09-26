511479ca67f6feedb2aae827124dab2835ab8aa0
Asiqur Rahman <asikur22@gmail.com>
2019-09-26 20:26:11 +0600

    Initialize

---
+++ index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const gulp = require("gulp");
+const image = require("gulp-image");
+const notify = require("gulp-notify");
+const rename = require("gulp-rename");
+const isImage = require("is-image");
+const chalk = require("chalk");
+const path = require("path");
+
+var images = process.argv.slice(2);
+var imgLinks = images.filter(img => isImage(img));
+if (imgLinks.length > 0) {
+	const newImages = imgLinks.map(img => path.resolve(__dirname, img));
+
+	// Copy file and rename.
+	gulp.src(newImages)
+		.pipe(rename({ suffix: "---old" }))
+		.pipe(
+			gulp.dest(function(file) {
+				return file.base;
+			})
+		);
+
+	// Minify Image
+	gulp.src(newImages)
+		.pipe(
+			image({
+				pngquant: true,
+				optipng: false,
+				zopflipng: true,
+				jpegRecompress: false,
+				mozjpeg: true,
+				guetzli: false,
+				gifsicle: true,
+				svgo: true,
+				concurrent: 10,
+				quiet: false // defaults to false
+			})
+		)
+		.pipe(
+			gulp.dest(function(file) {
+				return file.base;
+			})
+		)
+		.pipe(
+			notify({
+				title: "Gulp Image",
+				message: "<%= file.relative %> Compressed Successful",
+				wait: true
+			})
+		);
+} else {
+	console.log(chalk.black.bgRed("Plz Provide Valid Image"));
+}
