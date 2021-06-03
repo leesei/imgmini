3f5f31d165d93e3ec53856adc83c0d85d532f73d
leesei <leesei@gmail.com>
2021-06-03 14:41:59 +0800

    update command line
    Changes to be committed:
    	modified:   README.md
    	modified:   cli.js

--- README.md
+++ README.md
@@ -1,19 +1,36 @@
-# npm-imgmini
+# imgmini
 
 Image Minify with Gulp Image.
 
+Fork of [Asikur22/npm-imgmini](https://github.com/Asikur22/npm-imgmini), update dependencies and command line.
+
+Gulp 4 generates warning on usage of Chokidar v2, but we're not using this app for watch so it can be ignored.
+
+[imagemin/imagemin-cli](https://github.com/imagemin/imagemin-cli) is a modern incarnation, but:
+
+- it does not keep source's directory structure
+- the default invocation is not optimized
+- `Promise.all()` causes resource issue on low end machines (this app has concurrency limit)
+
 ## Install
 
-```console
-$ npm install -g imgmini
+```sh
+$ npm install -g @leesei/imgmini
 ```
 
 ## Usage
 
-```console
+```sh
 $ imgmini image.jpg image2.png
+# keep original files
+$ imgmini -k image.jpg image2.png
+
+# before globbing is suppported:
+find $FOLDER -type f | xargs -P$(nproc) -- imgmini
 ```
-#### Silent Mode
-```console
-$ imgmini -s image.jpg image2.png
+
+### Show System Notification
+
+```sh
+$ imgmini -n image.jpg image2.png
 ```
--- cli.js
+++ cli.js
@@ -6,73 +6,71 @@ const notify = require("gulp-notify");
 const rename = require("gulp-rename");
 const isImage = require("is-image");
 const gulpif = require("gulp-if");
-const isURL = require("is-url");
 const chalk = require("chalk");
 const path = require("path");
 const open = require("open");
 
 var args = process.argv.slice(2);
 
-// var argsLength = args.length;
-// var argsLast = args[args.length - 1];
-// var imagesURL = args.filter(arg => isURL(arg));
-
-var slient = args.includes("-s");
+var keepOld = args.includes("-k");
+var notification = args.includes("-n");
 var imgLinks = args.filter((arg) => isImage(arg));
 if (imgLinks.length > 0) {
-	const newImages = imgLinks.map((img) => path.resolve(process.cwd(), img));
+  const newImages = imgLinks.map((img) => path.resolve(process.cwd(), img));
 
-	// Copy file and rename.
-	if (!slient) {
-		gulp.src(newImages)
-			.pipe(rename({ suffix: "---old" }))
-			.pipe(
-				gulp.dest(function (file) {
-					return file.base;
-				})
-			);
-	}
+  // Copy file and rename.
+  if (keepOld) {
+    gulp
+      .src(newImages)
+      .pipe(rename({ suffix: "---old" }))
+      .pipe(
+        gulp.dest(function (file) {
+          return file.base;
+        })
+      );
+  }
 
-	// Minify Image
-	gulp.src(newImages)
-		.pipe(
-			image({
-				pngquant: true,
-				optipng: false,
-				zopflipng: true,
-				jpegRecompress: false,
-				mozjpeg: true,
-				guetzli: false,
-				gifsicle: true,
-				svgo: true,
-				concurrent: 10,
-				quiet: false, // defaults to false
-			})
-		)
-		.pipe(
-			gulp.dest(function (file) {
-				return file.base;
-			})
-		)
-		.pipe(
-			gulpif(
-				!slient,
-				notify(function (file) {
-					return {
-						title: "ImageMinify",
-						message: file.relative + " Compressed Successful",
-						actions: " Open Folder ",
-						wait: true,
-						folder: file.base,
-					};
-				})
-			)
-		);
+  // Minify Image
+  gulp
+    .src(newImages)
+    .pipe(
+      image({
+        pngquant: true,
+        optipng: false,
+        zopflipng: true,
+        jpegRecompress: false,
+        mozjpeg: true,
+        guetzli: false,
+        gifsicle: true,
+        svgo: true,
+        concurrent: 10,
+        quiet: false, // defaults to false
+      })
+    )
+    .pipe(
+      gulp.dest(function (file) {
+        return file.base;
+      })
+    )
+    .pipe(
+      gulpif(
+        notification,
+        notify(function (file) {
+          return {
+            title: "ImageMinify",
+            message: file.relative + " Compressed Successful",
+            actions: " Open Folder ",
+            wait: true,
+            folder: file.base,
+          };
+        })
+      )
+    );
 
-	// Show Notification
-	notify.on("click", function (options) {
-		open(options.folder);
-	});
+  // Show Notification
+  notify.on("click", function (options) {
+    open(options.folder);
+  });
 } else {
-	console.log(chalk.black.bgRed("Plz Provide Valid Image"));
+  console.log(chalk.black.bgRed("Plz Provide Valid Image"));
 }
